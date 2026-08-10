7a37cb0d7c313a8bf75b9e0b31f2384499b8d5d5
johnmsimo <johnmsimo@gmail.com>
2026-08-10 12:36:05 -0400

    fix(v4.5.0): normalize release file newlines

--- security.py
+++ security.py
@@ -80,7 +80,10 @@ def configure_security(app) -> None:
         g.enterprise_api_key = None
         supplied_api_key = request.headers.get("X-API-Key")
         if supplied_api_key:
-            if not (\n                request.path.startswith("/api/v4.4/")\n                or request.path.startswith("/api/v4.5/")\n            ):
+            if not (
+                request.path.startswith("/api/v4.4/")
+                or request.path.startswith("/api/v4.5/")
+            ):
                 return (
                     jsonify(
                         {
